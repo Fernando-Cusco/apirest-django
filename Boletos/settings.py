3f80de251bbf1ce2df06b245cf1238792d7f1f20
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-02-01 17:16:44 -0500

    db

--- Boletos/settings.py
+++ Boletos/settings.py
@@ -76,11 +76,15 @@ WSGI_APPLICATION = 'Boletos.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'apidjango',
+            'USER': 'admin',
+            'PASSWORD': 'admin',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
