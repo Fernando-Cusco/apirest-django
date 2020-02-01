f93292e985bbc9ffe66fc1c1d082b14a111802a3
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-02-01 17:13:15 -0500

    definimos los atributos de nuestro modelo

--- Avion/models.py
+++ Avion/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Avion(models.Model):
+
+    aerolinea = models.CharField(max_length=100)
+    fecha = models.DateField()
+    precio = models.IntegerField(default = 100)
+    descuento = models.IntegerField(default = 85)
